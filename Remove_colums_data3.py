455962a7c28c648f804e3a1647a69d35f77ecf25
Irnehs <henritschulz@gmail.com>
2025-02-24 17:11:44 -0800

    Updates to data cross-correlation. Need better/more features and way to categorize numerical values

---
+++ Remove_colums_data3.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Read the CSV file
+df = pd.read_csv('C:/Users/Karmanya Pandey/Desktop/UCSD/Winter Quarter 24/ECE 143/Project/data_files/new_data3.csv')
+
+# Drop the first column by index (0 represents the first column)
+df = df.drop(df.columns[0], axis=1)
+df = df.drop('mode', axis=1, errors='ignore')
+
+# Save the modified DataFrame to a new CSV file
+df.to_csv('C:/Users/Karmanya Pandey/Desktop/UCSD/Winter Quarter 24/ECE 143/Project/data_files/new_data3_updated.csv', index=False)
