ebe74f562dca5cd9bb5e209d05017b9f856abac2
Irnehs <henritschulz@gmail.com>
2025-02-24 17:24:29 -0800

    forgot to save merge results

--- Artist_extract_from_data2.py
+++ Artist_extract_from_data2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import ast
 
@@ -21,28 +20,4 @@ def extract_artist(artist_str):
 df['artists'] = df['artists'].apply(extract_artist)
 
 # Save the modified CSV
-=======
-import pandas as pd
-import ast
-
-# Load your CSV file
-input_file = 'C:/Users/Karmanya Pandey/Desktop/UCSD/Winter Quarter 24/ECE 143/Project/data_files/new_data2.csv'  # Your input CSV file name
-output_file = 'C:/Users/Karmanya Pandey/Desktop/UCSD/Winter Quarter 24/ECE 143/Project/data_files/new_data2_updated.csv'  # New CSV file with the popularity column
-df = pd.read_csv(input_file)
-
-# Function to extract the artist name from the dictionary-like string
-def extract_artist(artist_str):
-    try:
-        # Convert the string to a dictionary
-        artist_dict = ast.literal_eval(artist_str)
-        # Return the artist name (assuming the dictionary has one entry)
-        return list(artist_dict.values())[0]
-    except:
-        return artist_str  # In case the format isn't as expected
-
-# Apply the function to the 'artists' column
-df['artists'] = df['artists'].apply(extract_artist)
-
-# Save the modified CSV
->>>>>>> 65036df0fa401b2f794301a7f65372ab463333b2
 df.to_csv(output_file, index=False)
